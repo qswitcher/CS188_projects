56021d9a91e1efc21e548022d48dcf7056ab1e1a
Jeffrey Russom <jrussom@homeaway.com>
2013-12-26 20:18:47 -0600

    Finished question 6.

--- project3/reinforcement/analysis.py
+++ project3/reinforcement/analysis.py
@@ -56,9 +56,10 @@ def question3e():
     # If not possible, return 'NOT POSSIBLE'
 
 def question6():
-    answerEpsilon = None
-    answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    answerEpsilon = 0.1
+    answerLearningRate = 0.3
+    #return answerEpsilon, answerLearningRate
+    return 'NOT POSSIBLE'
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
